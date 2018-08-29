14767dc7dd6d3a8d5ca114a7b83afb1f60c2e3d9
Matheus Cruz <bromatheuscruz@gmail.com>
2018-08-29 19:13:55 -0300

    Limpando o ProjetomcApplication

--- src/main/java/br/com/matheuscruz/projetomc/ProjetomcApplication.java
+++ src/main/java/br/com/matheuscruz/projetomc/ProjetomcApplication.java
@@ -32,86 +32,14 @@ import br.com.matheuscruz.projetomc.repositories.ProdutoRepository;
 @SpringBootApplication
 public class ProjetomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private ProdutoRepository produtoRepository;
-	@Autowired
-	private EstadoRepository estadoRepository;
-	@Autowired
-	private CidadeRepository cidadeRepository;
-	@Autowired
-	private ClienteRepository clienteRepository;
-	@Autowired
-	private EnderecoRepository enderecoRepository;
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	@Autowired
-	private PagamentoRepository pagamentoRepository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		Categoria categoria1 = new Categoria(null, "Informática");
-		Categoria categoria2 = new Categoria(null, "Escritório");
-
-		Produto produto1 = new Produto(null, "Computador", 2000.00);
-		Produto produto2 = new Produto(null, "Impressora", 800.00);
-		Produto produto3 = new Produto(null, "Mouse", 80.00);
-
-		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
-		categoria2.getProdutos().addAll(Arrays.asList(produto2));
-
-		produto1.getCategorias().addAll(Arrays.asList(categoria1));
-		produto2.getCategorias().addAll(Arrays.asList(categoria2));
-		produto3.getCategorias().addAll(Arrays.asList(categoria1));
-
-		Estado estado1 = new Estado(null, "Minas Gerais");
-		Estado estado2 = new Estado(null, "São Paulo");
-
-		Cidade cidade1 = new Cidade(null, "Uberlandia", estado1);
-		Cidade cidade2 = new Cidade(null, "São Paulo", estado2);
-		Cidade cidade3 = new Cidade(null, "Campinas", estado1);
-
-		estado1.getCidades().addAll(Arrays.asList(cidade1));
-		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
-
-		Cliente cliente1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "123456789", TipoCliente.PESSOA_FISICA);
-		cliente1.getTelefones().addAll(Arrays.asList("2222-2222", "3333-3333"));
-
-		Endereco endereco1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cidade1,
-				cliente1);
-		Endereco endereco2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "387777012", cidade2,
-				cliente1);
-		cliente1.getEnderecos().addAll(Arrays.asList(endereco1, endereco2));
 		
-		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
-		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
-		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
-		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
-		clienteRepository.save(cliente1);
-		enderecoRepository.saveAll(Arrays.asList(endereco1, endereco2));
-
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-
-		Pedido pedido1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cliente1, endereco1);
-		Pedido pedido2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cliente1, endereco2);
-
-		Pagamento pagamento1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, pedido1, 6);
-		pedido1.setPagamento(pagamento1);
-
-		Pagamento pagamento2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, pedido2,
-				sdf.parse("20/10/2017 00:00"), null);
-		pedido2.setPagamento(pagamento2);
-
-		cliente1.getPedidos().addAll(Arrays.asList(pedido1, pedido2));
-
-		pedidoRepository.saveAll(Arrays.asList(pedido1, pedido2));
-		pagamentoRepository.saveAll(Arrays.asList(pagamento1, pagamento2));
+		//Cansei dessa bagunça ... rs
 
 	}
 }
--- src/main/java/br/com/matheuscruz/projetomc/domain/Pedido.java
+++ src/main/java/br/com/matheuscruz/projetomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package br.com.matheuscruz.projetomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -34,6 +37,9 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "endereco_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
 
@@ -75,6 +81,14 @@ public class Pedido implements Serializable {
 		return cliente;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
@@ -111,5 +125,6 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
 
 }
